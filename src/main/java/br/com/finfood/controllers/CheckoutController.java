9ee2c61af160270429d994ed0364a33c3a33957d
Vinicius Almeida <vgfontao@gmail.com>
2020-06-01 23:55:54 -0300

    Remove import

--- src/main/java/br/com/finfood/controllers/CheckoutController.java
+++ src/main/java/br/com/finfood/controllers/CheckoutController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-
 @Controller
 @RequestMapping("/checkout")
 public class CheckoutController {
