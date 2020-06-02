68b2d4689ecb66f31e717e2322b52a13d6f54920
Vinicius Almeida <vgfontao@gmail.com>
2020-06-01 21:59:02 -0300

    First commit

---
+++ src/main/java/br/com/finfood/FinfoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.finfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinfoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinfoodApplication.class, args);
+    }
+
+}
